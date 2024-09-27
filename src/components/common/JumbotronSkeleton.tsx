461936375951c6b80128294099fa83efb2f97c84
matthieuGravy <matthieugravy@gmail.com>
2024-09-27 18:17:35 +0200

    review-theme

--- src/components/common/JumbotronSkeleton.tsx
+++ src/components/common/JumbotronSkeleton.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const JumbotronSkeleton: React.FC = () => {
   return (
-    <div className="space-y-4">
-      <Skeleton className="h-12 w-3/4 mx-auto" />
-      <Skeleton className="h-4 w-full max-w-2xl mx-auto" />
-      <Skeleton className="h-4 w-full max-w-xl mx-auto" />
+    <div className="space-y-6 max-w-2xl mx-auto px-4 py-16">
+      <Skeleton className="h-10 w-3/4 mx-auto" />
+      <Skeleton className="h-4 w-full" />
+      <Skeleton className="h-4 w-5/6" />
       <div className="flex justify-center space-x-4 mt-8">
         <Skeleton className="h-10 w-32" />
         <Skeleton className="h-10 w-32" />
